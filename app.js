38fd717101fc9746124416bcc059bde5486ecc31
Binh Nguyen <nguyenthaibinh.bl@gmail.com>
2018-03-29 14:10:11 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const http = require('http');
 
 const server = express();
 
+const projectId = "small-talk-2-8260e"; //small talk agent Id
+const sessionId = 'quickstart-session-id';
+const dialogflow = require('dialogflow');
+const sessionClient = new dialogflow.SessionsClient();
+
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+const request = function(text) {
+    return {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: text()
+            }
+        }
+    }
+}
+
 server.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -14,7 +32,18 @@ server.use(morgan('combined'));
 server.use('/agent/routing',function(req,res,err) {
     console.log('req content ====> ',req.body);
 
-    res.json(req.body);
+    sessionClient.detectIntent(() => req.body.queryString)
+        .then(responses => {
+            const result = responses[0].queryResult;
+            console.log(`  Query: ${result.queryText}`);
+            console.log(`  Response: ${result.fulfillmentText}`);
+            if (result.intent) {
+              console.log(`  Intent: ${result.intent.displayName}`);
+            } else {
+              console.log(`  No intent matched.`);
+            }
+        })
+    res.json(responses);
 })
 
 server.listen((process.env.PORT || 3001),function() {

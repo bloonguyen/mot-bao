1b046066434d64c2e9e9f9a45f48373abc417a7c
Binh Nguyen <nguyenthaibinh.bl@gmail.com>
2018-04-04 17:03:52 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -2,53 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const http = require('http');
+const apiai = require('apiai');
 
 const server = express();
 
+const df = apiai('291a4a67331449f3b6cf3346f0e361bb');
 const projectId = "small-talk-2-8260e"; //small talk agent Id
 const sessionId = 'quickstart-session-id';
-var gcloud = require('google-cloud')({
-  projectId: projectId,
-
-  // The path to your key file:
-  keyFilename: './motBao-73970225caf1.json'
-});
-const Storage = require('@google-cloud/storage');
-
-const storage = new Storage();
-
-// Makes an authenticated API request.
-storage
-  .getBuckets()
-  .then((results) => {
-    const buckets = results[0];
-
-    console.log('Buckets:');
-    buckets.forEach((bucket) => {
-      console.log(bucket.name);
-    });
-  })
-  .catch((err) => {
-    console.error('ERROR:', err);
-  });
-
-
-const dialogflow = require('dialogflow');
-const sessionClient = new dialogflow.SessionsClient();
-
-
-const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
-const request = function(text) {
-    return {
-        session: sessionPath,
-        queryInput: {
-            text: {
-                text: text()
-            }
-        }
-    }
-}
 
 server.use(bodyParser.urlencoded({
     extended: true
@@ -56,21 +16,32 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 server.use(morgan('combined'));
 
-server.use('/agent/routing',function(req,res,err) {
-    console.log('req content ====> ',req.body);
 
-    sessionClient.detectIntent(() => req.body.queryString)
-        .then(responses => {
-            const result = responses[0].queryResult;
-            console.log(`  Query: ${result.queryText}`);
-            console.log(`  Response: ${result.fulfillmentText}`);
-            if (result.intent) {
-              console.log(`  Intent: ${result.intent.displayName}`);
-            } else {
-              console.log(`  No intent matched.`);
-            }
-            res.json(responses);
-        })
+function dfRequest(query) {
+    return new Promise(function(resolve,reject) {
+        var request = df.textRequest(query, {
+            sessionId: Math.random().toString().slice(2)
+        });
+
+        request.on('response', function(response) {
+            return resolve(response);
+        });
+
+        request.on('error', function(error) {
+            return reject(response);
+        });
+
+        request.end();
+    })
+}
+
+server.use('/agent/routing',function(req,res,err) {
+    dfRequest(req.body.queryString).then(function(response) {
+        var messages = [];
+        messages.push({text:response.result.fulfillment.speech});
+        res.json({messages:messages});
+    })
+
 })
 
 server.listen((process.env.PORT || 3001),function() {

87aa108d50fb33af37bcde493aec1856956e5c50
Binh Nguyen <nguyenthaibinh.bl@gmail.com>
2018-05-14 10:25:58 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const http = require('http');
 const apiai = require('apiai');
+const Parser = require('rss-parser');
 
 const server = express();
 
@@ -35,6 +36,7 @@ function dfRequest(query) {
     })
 }
 
+// call Dialogflow smalltalk agent
 server.use('/agent/routing',function(req,res,err) {
     dfRequest(req.body.queryString).then(function(response) {
         var messages = [];
@@ -42,6 +44,39 @@ server.use('/agent/routing',function(req,res,err) {
         res.json({messages:messages});
     })
 
+})
+
+server.use('/trending',function(req,res,err) {
+    var parser = new Parser({});
+    parser.parseURL('https://trends.google.com/trends/hottrends/atom/feed?pn=p28', function(err,feed) {
+        if (err) {
+            res.json(err);
+        }
+        else {
+            console.log(feed.items);
+            var messages = [];
+            feed.items.forEach(function(item,index) {
+                console.log('forEach ===>',item,index);
+                if (index < 5) {
+                    if (item.title!='') {
+                        messages.push({text:item.title});
+                    }
+                    if (item.content!='') {
+                        messages.push({text:item.content});
+                    }
+                    if (item.contentSnippet!='') {
+                        messages.push({text:item.contentSnippet});
+                    }
+                }
+                else return;
+            });
+            if (messages.length >= 10) {
+                messages = messages.slice(8);
+            }
+            var jsonResponse = {messages:messages};
+            res.json(jsonResponse);
+        }
+    })
 })
 
 server.listen((process.env.PORT || 3001),function() {
